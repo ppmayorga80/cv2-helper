a6de1f6b2a34f50fabc7bd9d1656b1deec39c011
Pedro Mayorga <ppmayorga80@gmail.com>
2026-09-22 03:56:19 -0600

    feat: wait_key implementation (1st version)

--- cv2_helper/__init__.py
+++ cv2_helper/__init__.py
@@ -1,6 +1,6 @@
 """cv2_helper package initialization."""
 
 from .core import add_borders, add_labels, resize, imshow
-from ._types import TextPos, TextAttr
+from ._types import TextPos, TextAttr, FitOption, KeyCode
 
 __version__ = "1.2.0"
